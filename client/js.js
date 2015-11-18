c431feb43d3bb1cf21c617e964c179c5ed305335
Nasir <zman2712@gmail.com>
2015-11-17 20:04:55 -0500

    Added js to js files

--- client/js.js
+++ client/js.js
@@ -1,23 +1,8 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
 
   Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+    "click button":function(event, template){
+     console.log('hello World!');
     }
   });
-}
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+
