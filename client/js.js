13561fa55225ab6245ad454bb02a5fdc6ad9a647
garryd635 <garryd635@gmail.com>
2015-11-19 18:32:33 -0500

    madLib sentence working

--- client/js.js
+++ client/js.js
@@ -1,20 +1,22 @@
 Meteor.startup(function() {
-
-
-  Template.hello.events({
-
-    'click button': function () {
-      // increment the counter when button is clicked
-      console.log("Hello,World");
-      // Session.set('counter', Session.get('counter') + 1);
-
+  $('#mad-lib-sentence').hide()
   $("#generateButton").click(function(event) {
   event.preventDefault();
   var exclamation = $('#exclamation').val();
   var adverb = $('#adverb').val();
   var noun = $('#noun').val();;
   var verb = $('#verb').val();;
+  console.log(exclamation);
   console.log(adverb);
+  console.log(noun);
+  console.log(verb);
+
+  $('#exclamation-mad-lib').html(exclamation)
+  $('#adverb-mad-lib').html(adverb)
+  $('#noun-mad-lib').html(noun)
+  $('#verb-mad-lib').html(verb)
+
+  $('#mad-lib-sentence').fadeIn("slow")
 
   });
 
